1956fe052eac87104aee5b1a8033b079292a5653
Emma Shih <emma.shih@shopee.com>
2021-02-24 21:01:08 +0800

    18 Feb 2021

--- Two Pointers/Squaring-a-Sorted-Array(E).js
+++ Two Pointers/Squaring-a-Sorted-Array(E).js
@@ -1,26 +1,26 @@
 const make_squares = function (arr) {
-	squares = []
-	// TODO: Write your code here
-	const n = arr.length
-	squares = Array(n).fill(0)
-	let biggestIdx = n - 1
-	let right = n - 1
-	let left = 0
+  squares = []
+  // TODO: Write your code here
+  const n = arr.length
+  squares = Array(n).fill(0)
+  let biggestIdx = n - 1
+  let right = n - 1
+  let left = 0
 
-	while (left < right) {
-		let squareRightNum = arr[right] * arr[right]
-		let squareLeftNum = arr[left] * arr[left]
+  while (left < right) {
+    let squareRightNum = arr[right] * arr[right]
+    let squareLeftNum = arr[left] * arr[left]
 
-		if (squareLeftNum > squareRightNum) {
-			squares[biggestIdx] = squareLeftNum
-			left += 1
-		} else {
-			squares[biggestIdx] = squareRightNum
-			right -= 1
-		}
+    if (squareLeftNum > squareRightNum) {
+      squares[biggestIdx] = squareLeftNum
+      left += 1
+    } else {
+      squares[biggestIdx] = squareRightNum
+      right -= 1
+    }
 
-		biggestIdx -= 1
-	}
+    biggestIdx -= 1
+  }
 
-	return squares;
-};
+  return squares
+}
